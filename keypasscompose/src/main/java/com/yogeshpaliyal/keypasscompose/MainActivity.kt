e881104f833a70c3196b0d04f380379103fc6b0b
Yogesh Choudhary Paliyal <paliyalyogesh@gmail.com>
2021-11-13 18:11:42 +0530

    [Compose] Add Scaffold, Bottom Toolbar and Fab

--- keypasscompose/src/main/java/com/yogeshpaliyal/keypasscompose/MainActivity.kt
+++ keypasscompose/src/main/java/com/yogeshpaliyal/keypasscompose/MainActivity.kt
@@ -3,20 +3,63 @@ package com.yogeshpaliyal.keypasscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.FabPosition
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.yogeshpaliyal.keypasscompose.ui.theme.KeyPassTheme
+import com.yogeshpaliyal.keypasscompose.ui.theme.Material3BottomAppBar
+import com.yogeshpaliyal.keypasscompose.ui.theme.Material3Scaffold
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             KeyPassTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Material3Scaffold(bottomBar = {
+                        Material3BottomAppBar(cutoutShape = RoundedCornerShape(50)) {
+                            IconButton(
+                                onClick = {
+                                    /* doSomething() */
+                                }
+                            ) {
+                                Icon(Icons.Filled.Menu,"")
+                            }
+
+                            Spacer(Modifier.weight(1f, true))
+                            IconButton(
+                                onClick = {
+                                    /* doSomething() */
+                                }
+                            ) {
+                                Icon(Icons.Filled.Star,"")
+                            }
+                        }
+                    }, floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = {
+
+                            },
+                            contentColor = Color.White,
+                            shape = RoundedCornerShape(50)
+                        ) {
+                            Icon(Icons.Filled.Add,"")
+                        }
+                    }, floatingActionButtonPosition = FabPosition.Center,
+                    isFloatingActionButtonDocked = true) {
+
+                    }
                     Greeting("Android")
                 }
             }
--- keypasscompose/src/main/java/com/yogeshpaliyal/keypasscompose/ui/theme/Theme.kt
+++ keypasscompose/src/main/java/com/yogeshpaliyal/keypasscompose/ui/theme/Theme.kt
@@ -1,20 +1,20 @@
 package com.yogeshpaliyal.keypasscompose.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 
-private val DarkColorPalette = darkColors(
+private val DarkColorPalette = darkColorScheme(
     primary = Purple200,
-    primaryVariant = Purple700,
+    primaryContainer = Purple700,
     secondary = Teal200
 )
 
-private val LightColorPalette = lightColors(
+private val LightColorPalette = lightColorScheme(
     primary = Purple500,
-    primaryVariant = Purple700,
+    primaryContainer = Purple700,
     secondary = Teal200
 
     /* Other default colors to override
@@ -36,9 +36,8 @@ fun KeyPassTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composabl
     }
 
     MaterialTheme(
-        colors = colors,
+        colorScheme = colors,
         typography = Typography,
-        shapes = Shapes,
         content = content
     )
 }
