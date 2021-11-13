da238f0bcd0b70dde067efa34b99089c76c9cf83
Yogesh Choudhary Paliyal <paliyalyogesh@gmail.com>
2021-11-13 15:52:01 +0530

    Create compose module

---
+++ keypasscompose/src/main/java/com/yogeshpaliyal/keypasscompose/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.yogeshpaliyal.keypasscompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.yogeshpaliyal.keypasscompose.ui.theme.KeyPassTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KeyPassTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    KeyPassTheme {
+        Greeting("Android")
+    }
+}
