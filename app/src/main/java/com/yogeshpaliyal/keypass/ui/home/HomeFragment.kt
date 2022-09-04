a80d91d2302c03e93e14de1429cd0bf6187f7c3a
Yogesh Choudhary Paliyal <paliyalyogesh@gmail.com>
2022-09-04 21:27:23 +0530

    fix spotless

--- app/src/main/java/com/yogeshpaliyal/keypass/ui/home/HomeFragment.kt
+++ app/src/main/java/com/yogeshpaliyal/keypass/ui/home/HomeFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
