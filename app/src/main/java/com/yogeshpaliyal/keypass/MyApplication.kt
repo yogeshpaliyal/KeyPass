00786ae844557f5cf03dc7647dc51e429fd3ce9d
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2023-01-08 19:27:34 +0530

    fix: remove unused imports and add last line

--- app/src/main/java/com/yogeshpaliyal/keypass/MyApplication.kt
+++ app/src/main/java/com/yogeshpaliyal/keypass/MyApplication.kt
@@ -1,16 +1,9 @@
 package com.yogeshpaliyal.keypass
 
-import android.app.Application
 import android.content.Intent
-import android.util.Log
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
-import com.google.android.material.color.DynamicColors
 import com.yogeshpaliyal.common.CommonMyApplication
 import com.yogeshpaliyal.keypass.ui.CrashActivity
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
-import kotlin.system.exitProcess
 
 /*
 * @author Yogesh Paliyal
--- common/src/main/java/com/yogeshpaliyal/common/CommonMyApplication.kt
+++ common/src/main/java/com/yogeshpaliyal/common/CommonMyApplication.kt
@@ -6,7 +6,6 @@ import android.util.Log
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import com.google.android.material.color.DynamicColors
-import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 import kotlin.system.exitProcess
 
