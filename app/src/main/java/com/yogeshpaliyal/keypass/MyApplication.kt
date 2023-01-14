872098a04e09855d641b4bd7e9faad3bc7ec7207
Yogesh Choudhary Paliyal <paliyalyogesh@gmail.com>
2023-01-14 17:16:37 +0530

    create common application class, fix fab styling (#287)
    * create common application class, fix fab styling
    
    * fix: remove unused imports and add last line
    
    * fix: spotless issues
    
    * common application changes

--- app/src/main/java/com/yogeshpaliyal/keypass/MyApplication.kt
+++ app/src/main/java/com/yogeshpaliyal/keypass/MyApplication.kt
@@ -1,15 +1,9 @@
 package com.yogeshpaliyal.keypass
 
-import android.app.Application
 import android.content.Intent
-import android.util.Log
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
-import com.google.android.material.color.DynamicColors
+import com.yogeshpaliyal.common.CommonMyApplication
 import com.yogeshpaliyal.keypass.ui.CrashActivity
 import dagger.hilt.android.HiltAndroidApp
-import javax.inject.Inject
-import kotlin.system.exitProcess
 
 /*
 * @author Yogesh Paliyal
@@ -18,28 +12,9 @@ import kotlin.system.exitProcess
 * created on 22-01-2021 22:41
 */
 @HiltAndroidApp
-class MyApplication : Application(), Configuration.Provider {
+class MyApplication : CommonMyApplication() {
 
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
-
-    override fun onCreate() {
-        super.onCreate()
-        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
-            Log.d("MyApplication", "crashed ")
-            val intent = CrashActivity.getIntent(this, throwable.localizedMessage)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-            startActivity(intent)
-            exitProcess(1)
-        }
-
-        DynamicColors.applyToActivitiesIfAvailable(this)
-    }
-
-    override fun getWorkManagerConfiguration(): Configuration {
-        return Configuration.Builder()
-            .setMinimumLoggingLevel(android.util.Log.INFO)
-            .setWorkerFactory(workerFactory)
-            .build()
+    override fun getCrashActivityIntent(throwable: Throwable): Intent {
+        return CrashActivity.getIntent(this, throwable.localizedMessage)
     }
 }
