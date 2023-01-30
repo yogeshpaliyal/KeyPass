00d15f72ed4ed246786d9c89969d9b86cd05268d
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-01-31 03:15:18 +0530

    Remove xml layout file.

--- keypasscompose/src/main/java/com/yogeshpaliyal/keypasscompose/ui/generate/GeneratePasswordActivity.kt
+++ keypasscompose/src/main/java/com/yogeshpaliyal/keypasscompose/ui/generate/GeneratePasswordActivity.kt
@@ -5,20 +5,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.google.accompanist.themeadapter.material3.Mdc3Theme
-import com.yogeshpaliyal.keypasscompose.databinding.ActivityGeneratePasswordBinding
 import com.yogeshpaliyal.keypasscompose.ui.generate.ui.GeneratePasswordScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GeneratePasswordActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityGeneratePasswordBinding
-
     private val viewModel: GeneratePasswordViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityGeneratePasswordBinding.inflate(layoutInflater)
-
         setContent {
             Mdc3Theme {
                 GeneratePasswordScreen(viewModel)
