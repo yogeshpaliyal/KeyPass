9a2917f9a2ff856c5fa5e8d8c14816fe80d6cc63
Abhishek Saxena <5abhisheksaxena@gmail.com>
2023-01-31 22:41:53 +0530

    Migrate GeneratePasswordActivity to compose. (#314)
    * Move password generation logic to ViewModel.
    
    * Store GeneratePassword state in GeneratePasswordViewState.
    
    * Update GeneratePasswordState when form is updated.
    
    * Remove password StateFlow nad use the one in the ViewState.
    
    * Fix dependency version incompatibility issue and add missing dependencies.
    
    * Add mdc3Theme adapter.
    
    * Add GeneratePasswordContent form.
    
    * Integrate GeneratePasswordContent.
    
    * Fix spotless issues.
    
    * Remove xml layout file.

---
+++ keypasscompose/src/main/java/com/yogeshpaliyal/keypasscompose/ui/generate/GeneratePasswordViewModel.kt
@@ -0,0 +1,63 @@
+package com.yogeshpaliyal.keypasscompose.ui.generate
+
+import androidx.lifecycle.ViewModel
+import com.yogeshpaliyal.common.utils.PasswordGenerator
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
+
+@HiltViewModel
+class GeneratePasswordViewModel @Inject constructor() : ViewModel() {
+
+    private val _viewState = MutableStateFlow(GeneratePasswordViewState.Initial)
+    val viewState = _viewState.asStateFlow()
+
+    fun generatePassword() {
+        val currentViewState = _viewState.value
+
+        val passwordGenerator = PasswordGenerator(
+            length = currentViewState.length,
+            includeUpperCaseLetters = currentViewState.includeUppercaseLetters,
+            includeLowerCaseLetters = currentViewState.includeLowercaseLetters,
+            includeSymbols = currentViewState.includeSymbols,
+            includeNumbers = currentViewState.includeNumbers,
+        )
+
+        _viewState.update {
+            val newPassword = passwordGenerator.generatePassword()
+            it.copy(password = newPassword)
+        }
+    }
+
+    fun onPasswordLengthSliderChange(value: Float) {
+        _viewState.update {
+            it.copy(length = value.toInt())
+        }
+    }
+
+    fun onUppercaseCheckedChange(checked: Boolean) {
+        _viewState.update {
+            it.copy(includeUppercaseLetters = checked)
+        }
+    }
+
+    fun onLowercaseCheckedChange(checked: Boolean) {
+        _viewState.update {
+            it.copy(includeLowercaseLetters = checked)
+        }
+    }
+
+    fun onNumbersCheckedChange(checked: Boolean) {
+        _viewState.update {
+            it.copy(includeNumbers = checked)
+        }
+    }
+
+    fun onSymbolsCheckedChange(checked: Boolean) {
+        _viewState.update {
+            it.copy(includeSymbols = checked)
+        }
+    }
+}
