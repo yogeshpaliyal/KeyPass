b53e320216a1b2924eecb74c057a0d60b4550b24
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2023-02-27 00:06:23 +0530

    removed xml app package

---
+++ app/src/main/java/com/yogeshpaliyal/keypass/ui/generate/GeneratePasswordViewState.kt
@@ -0,0 +1,21 @@
+package com.yogeshpaliyal.keypass.ui.generate
+
+data class GeneratePasswordViewState(
+    val length: Int,
+    val includeUppercaseLetters: Boolean,
+    val includeLowercaseLetters: Boolean,
+    val includeSymbols: Boolean,
+    val includeNumbers: Boolean,
+    val password: String,
+) {
+    companion object {
+        val Initial = GeneratePasswordViewState(
+            length = 10,
+            includeUppercaseLetters = true,
+            includeLowercaseLetters = true,
+            includeSymbols = true,
+            includeNumbers = true,
+            password = ""
+        )
+    }
+}
