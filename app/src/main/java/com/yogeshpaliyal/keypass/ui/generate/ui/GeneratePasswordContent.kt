da4427a60f512c065acb5cf40bc1e575cd120d61
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2023-02-27 22:11:43 +0530

    fix: spotless

--- app/src/main/java/com/yogeshpaliyal/keypass/ui/generate/ui/GeneratePasswordContent.kt
+++ app/src/main/java/com/yogeshpaliyal/keypass/ui/generate/ui/GeneratePasswordContent.kt
@@ -1,18 +1,15 @@
 package com.yogeshpaliyal.keypass.ui.generate.ui
 
 import android.content.res.Configuration
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ContentCopy
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material.primarySurface
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -21,11 +18,9 @@ import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.accompanist.themeadapter.material3.Mdc3Theme
@@ -218,15 +213,15 @@ private fun GeneratePasswordContentPreview() {
     val viewState = GeneratePasswordViewState.Initial
 
     Mdc3Theme {
-            GeneratePasswordContent(
-                viewState = viewState,
-                onGeneratePasswordClick = {},
-                onCopyPasswordClick = {},
-                onPasswordLengthChange = {},
-                onUppercaseCheckedChange = {},
-                onLowercaseCheckedChange = {},
-                onNumbersCheckedChange = {},
-                onSymbolsCheckedChange = {}
-            )
+        GeneratePasswordContent(
+            viewState = viewState,
+            onGeneratePasswordClick = {},
+            onCopyPasswordClick = {},
+            onPasswordLengthChange = {},
+            onUppercaseCheckedChange = {},
+            onLowercaseCheckedChange = {},
+            onNumbersCheckedChange = {},
+            onSymbolsCheckedChange = {}
+        )
     }
 }
