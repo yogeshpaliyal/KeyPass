556cb6d1ac799e2e010777954b4faa25cc38bab4
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2023-04-29 16:31:10 +0530

    Restore common/src/main/java/com/yogeshpaliyal/common/di/module/AppModule.kt

--- common/src/main/java/com/yogeshpaliyal/common/di/module/AppModule.kt
+++ common/src/main/java/com/yogeshpaliyal/common/di/module/AppModule.kt
@@ -1,9 +1,7 @@
 package com.yogeshpaliyal.common.di.module
 
 import android.content.Context
-import android.util.Log
 import androidx.room.Room
-import androidx.room.RoomDatabase
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 import com.yogeshpaliyal.common.AppDatabase
@@ -17,7 +15,6 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import java.util.concurrent.Executors
 import javax.inject.Singleton
 
 @Module
@@ -27,37 +24,27 @@ object AppModule {
     @Provides
     @Singleton
     fun getDb(@ApplicationContext context: Context): AppDatabase {
-        val builder = Room.databaseBuilder(
+        return Room.databaseBuilder(
             context,
             AppDatabase::class.java,
             context.getString(R.string.app_name)
-        )
-        builder.setQueryCallback(SqliteDatabaseLogger(), Executors.newSingleThreadExecutor())
-        builder.addMigrations(object : Migration(DB_VERSION_3, DB_VERSION_4) {
+        ).addMigrations(object : Migration(DB_VERSION_3, DB_VERSION_4) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("ALTER TABLE `account` ADD COLUMN `unique_id` TEXT")
                 database.query("select id,unique_id from `account` where unique_id IS NULL")
-                    .use {
+                    ?.use {
                         while (it.moveToNext()) {
                             val id = it.getInt(0)
-                            val query =
-                                "update `account` set `unique_id` = '${getRandomString()}' where `id` = '$id'"
+                            val query = "update `account` set `unique_id` = '${getRandomString()}' where `id` = '$id'"
                             database.execSQL(query)
                         }
                     }
             }
-        })
-        builder.addMigrations(object : Migration(DB_VERSION_4, DB_VERSION_5) {
+        }).addMigrations(object : Migration(DB_VERSION_4, DB_VERSION_5) {
             override fun migrate(database: SupportSQLiteDatabase) {
                 database.execSQL("ALTER TABLE `account` ADD COLUMN `type` INT DEFAULT 0")
             }
         })
-        return builder.build()
-    }
-}
-
-class SqliteDatabaseLogger : RoomDatabase.QueryCallback {
-    override fun onQuery(sqlQuery: String, bindArgs: List<Any?>) {
-        Log.d("QueryUpdated", "SQL Query: $sqlQuery SQL Args: $bindArgs")
+            .build()
     }
 }
