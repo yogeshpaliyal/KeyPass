cd0a9af0bf4fa6ffb197d9d999afe96fa9206430
rohitashwani1 <rohitas@iitbhilai.ac.in>
2023-05-08 21:01:28 +0530

    Issue #477 resolved

--- app/src/main/java/com/yogeshpaliyal/keypass/ui/redux/KeyPassRedux.kt
+++ app/src/main/java/com/yogeshpaliyal/keypass/ui/redux/KeyPassRedux.kt
@@ -33,8 +33,8 @@ object KeyPassRedux {
                 } else {
                     arrPages.add(state.currentScreen)
                 }
-                if(state.currentScreen == action.state){
-                    arrPages.remove(state.currentScreen);
+                if (state.currentScreen == action.state) {
+                    arrPages.remove(state.currentScreen)
                 }
                 state.copy(currentScreen = action.state)
             }
