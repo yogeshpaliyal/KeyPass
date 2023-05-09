f361177e5328ffabf9aa831432b8ef16571f248e
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2023-05-09 23:24:03 +0530

    script changes

---
+++ scripts/masterVersion.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Read the current versionCode and versionName from the build.gradle file
+VERSION_NAME=$(grep "versionName" app/build.gradle | awk '{print $2}' | tr -d '\r''"')
+
+
+NEW_VERSION_NAME="$VERSION_NAME-master"
+
+# Update the build.gradle file with the new versionCode and versionName values
+sed -i "s/versionName \"$VERSION_NAME\"/versionName \"$NEW_VERSION_NAME\"/" app/build.gradle
+
+# Output the new versionCode and versionName values
+echo "v$NEW_VERSION_NAME"
