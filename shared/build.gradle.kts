8d655e6ca4d18b9680cf534de056dbf3b7c040b2
Yogesh Choudhary Paliyal <paliyalyogesh@gmail.com>
2024-05-19 14:41:59 +0530

    Kotlin bump (#851)
    * feat: enable lint error
    
    * feat: Kotlin Bump
    
    * feat: kotlin compose compatibility upgrade

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -29,7 +29,7 @@ kotlin {
                 api("androidx.core:core-ktx:1.13.1")
             }
         }
-        val androidTest by getting {
+        val androidUnitTest by getting {
             dependencies {
                 implementation("junit:junit:4.13.2")
             }
