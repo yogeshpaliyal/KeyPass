4f20ea35796709f156aa0574558fa52a4410a4ce
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2024-05-19 12:25:02 +0530

    feat: kotlin compose compatibility upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,7 +73,7 @@ android {
 
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.4"
+        kotlinCompilerExtensionVersion = "1.5.13"
     }
     packagingOptions {
         resources {
