d7e25a4f7523a40fc2d576b22048acdf465b929a
Kapil Baser <94954247+kapilBaser@users.noreply.github.com>
2024-06-07 22:59:56 +0530

    Added Description to create password screen (#871)

--- app/src/main/java/com/yogeshpaliyal/keypass/ui/redux/states/AuthState.kt
+++ app/src/main/java/com/yogeshpaliyal/keypass/ui/redux/states/AuthState.kt
@@ -3,8 +3,8 @@ package com.yogeshpaliyal.keypass.ui.redux.states
 import androidx.annotation.StringRes
 import com.yogeshpaliyal.keypass.R
 
-open class AuthState(@StringRes val title: Int) : ScreenState(false) {
-    object CreatePassword : AuthState(R.string.create_password)
-    class ConfirmPassword(val password: String) : AuthState(R.string.confirm_password)
+open class AuthState(@StringRes val title: Int, @StringRes val description: Int? = null) : ScreenState(false) {
+    object CreatePassword : AuthState(R.string.create_password, R.string.create_password_description)
+    class ConfirmPassword(val password: String) : AuthState(R.string.confirm_password, R.string.confirm_password_description)
     object Login : AuthState(R.string.login_to_enter_keypass)
 }
