518fc24c2a20585dc0f2e9fda682430047bfdf58
Yogesh Choudhary Paliyal <yogeshpaliyal@users.noreply.github.com>
2025-01-25 17:07:31 +0000

    Github Actions: App version Bumped to v1.4.34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = appPackageId
         minSdk = 23
         targetSdk = 35
-        versionCode = 1433
-        versionName = "1.4.33"
+        versionCode = 1434
+        versionName = "1.4.34"
 
         testInstrumentationRunner = "com.yogeshpaliyal.keypass.CustomTestRunner"
         vectorDrawables {
