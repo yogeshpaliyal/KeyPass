4d0a2c328635fc0b1a5ce5a645a2e49514ec6014
Yogesh Choudhary Paliyal <yogeshpaliyal.foss@gmail.com>
2025-01-26 14:25:50 +0530

    feat: batch actions

--- app/src/main/java/com/yogeshpaliyal/keypass/ui/redux/actions/IntentNavigation.kt
+++ app/src/main/java/com/yogeshpaliyal/keypass/ui/redux/actions/IntentNavigation.kt
@@ -1,7 +1,10 @@
 package com.yogeshpaliyal.keypass.ui.redux.actions
 
+/**
+ * Intent Navigation actions, used to navigate to different activities
+ * handled in {@link IntentNavigationMiddleware}
+ */
 sealed interface IntentNavigation : Action {
     object GeneratePassword : IntentNavigation
-    object BackupActivity : IntentNavigation
     object ShareApp : IntentNavigation
 }
