7b8410be1899241be057da4dd24056d7b456caa1
Yogesh Choudhary Paliyal <paliyalyogesh@gmail.com>
2025-04-10 23:31:03 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = appPackageId
         minSdk = 23
         targetSdk = 35
-        versionCode = 1438
-        versionName = "1.4.38"
+        versionCode = 1436
+        versionName = "1.4.36"
 
         testInstrumentationRunner = "com.yogeshpaliyal.keypass.CustomTestRunner"
         vectorDrawables {
